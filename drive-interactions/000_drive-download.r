1264b833130ac6bc469ffcae86f65c4ae05bef77
njlyon0 <nicholasjlyon@gmail.com>
2025-12-16 11:02:07 -0500

    feat (drive): narrowing down files to download

--- drive-interactions/000_drive-download.r
+++ drive-interactions/000_drive-download.r
@@ -22,11 +22,14 @@ rm(list = ls()); gc()
 link_lvl0 <- googledrive::as_id("https://drive.google.com/drive/folders/1EuicPFFm20bE16v9Z-_0OHv2gyQqdbah")
 
 # Check what's in that
-(drive_lvl0 <- googledrive::drive_ls(path = link_lvl0))
+(drive_lvl0 <- googledrive::drive_ls(path = link_lvl0) %>% 
+  ## And pare down to only the desired files
+  dplyr::filter(stringr::str_detect(string = name, pattern = "long|addspp") != T) %>% 
+  dplyr::filter(stringr::str_detect(string = tolower(name), pattern = "trex|midland")))
 
 # Download all the contents of that
 purrr::walk2(.x = drive_lvl0$id, .y = drive_lvl0$name,
   .f = ~ googledrive::drive_download(file = .x, overwrite = T,
-    path = file.path("data", "level-0", .y)))
+    path = file.path("data", "level-0", tolower(.y))))
 
 # End ----
